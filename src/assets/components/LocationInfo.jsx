bd865840f5347bdd39cb683b134274e5ebf2480f
Erika Vazquez Perez <evazquezperez@local.jaeoregon.com>
2023-02-07 08:50:01 -0800

    7

--- src/assets/components/LocationInfo.jsx
+++ src/assets/components/LocationInfo.jsx
@@ -6,10 +6,10 @@ const LocationInfo = ({location}) => {
     <section className='location_dad'>
     
     <ul className='location_ul'>
-    <li className='location_item'><span>Name:</span> {location?.name} </li>
-    <li className='location_item'><span>Type: </span>{location?.type}</li>
-    <li className='location_item'><span>Dimension: </span>{location?.dimension}</li>
-    <li className='location_item'><span>Population: </span>{location?.residents.length}</li></ul>
+    <li className='location_item a'><span>Name:</span> {location?.name} </li>
+    <li className='location_item b'><span>Type: </span>{location?.type}</li>
+    <li className='location_item c'><span>Dimension: </span>{location?.dimension}</li>
+    <li className='location_item d'><span>Population: </span>{location?.residents.length}</li></ul>
    </section>
    </div>
   )
