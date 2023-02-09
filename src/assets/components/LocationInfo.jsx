f9d592c68c2d3c53233645fe92dd4d953e481c84
Erika Vazquez Perez <evazquezperez@local.jaeoregon.com>
2023-02-09 14:02:59 -0800

    7

--- src/assets/components/LocationInfo.jsx
+++ src/assets/components/LocationInfo.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './styles/LocationInfo.css'
-const LocationInfo = ({location}) => {
+const LocationInfo = ({ location }) => {
   return (
     <div className='location_dad_nav'>
-    <section className='location_dad'>
-    
-    <ul className='location_ul'>
-    <li className='location_item a'><span>Name:</span> {location?.name} </li>
-    <li className='location_item b'><span>Type: </span>{location?.type}</li>
-    <li className='location_item c'><span>Dimension: </span>{location?.dimension}</li>
-    <li className='location_item d'><span>Population: </span>{location?.residents.length}</li></ul>
-   </section>
-   </div>
+      <section className='location_dad'>
+
+        <ul className='location_ul'>
+          <li className='location_item '><span>Name:</span> {location?.name} </li>
+          <li className='location_item '><span>Type: </span>{location?.type}</li>
+          <li className='location_item '><span>Dimension: </span>{location?.dimension}</li>
+          <li className='location_item '><span>Population: </span>{location?.residents.length}</li></ul>
+      </section>
+    </div>
   )
 }
 
